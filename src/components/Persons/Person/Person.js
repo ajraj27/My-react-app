e5fed0362ca7f57aabd8a283768500dd2e4a386d
ajraj27 <rajanuj.iiita@gmail.com>
2018-12-28 02:05:36 +0530

    break down into more refined components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,8 +5,7 @@ import classes from './Person.css';
 const person = ( props ) => {
     return (
         <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
+            <p onClick={props.clicked}>I'm {props.name} and I am {props.age} years old!</p>
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
